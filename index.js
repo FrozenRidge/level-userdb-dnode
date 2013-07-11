fae5845b10914227b1eea085f6c721c51fe8dd04
Niall O'Higgins <niallo@frozenridge.co>
2013-07-11 17:15:58 -0600

    instrument with log events

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var dnode = require('dnode')
 var db = require('level-userdb')
+var EventEmitter = require('events').EventEmitter
 
 module.exports = {
   // Parameter is either an already-created level-userdb object or a string to pass as
@@ -8,28 +9,35 @@ module.exports = {
     if (typeof dbi === 'string') {
       dbi = db(dbi)
     }
+    var emitter = new EventEmitter()
     var s = dnode({
       findUser: function(email, cb) {
+        emitter.emit('log', {email:email, method:'findUser'})
         dbi.findUser(email, cb)
       },
 
       addUser: function(email, password, data, cb) {
+        emitter.emit('log', {email:email, method:'addUser', data:data, password:password})
         dbi.addUser(email, password, data, cb)
       },
 
       checkPassword: function(email, password, cb) {
+        emitter.emit('log', {email:email, method:'checkPassword', password:password})
         dbi.checkPassword(email, password, cb)
       },
 
       changeEmail: function(email, newEmail, cb) {
+        emitter.emit('log', {email:email, newEmail:newEmail, method:'changeEmail'})
         dbi.changeEmail(email, newEmail, cb)
       },
 
       deleteUser: function(email, cb) {
+        emitter.emit('log', {email:email, method:'deleteUser'})
         dbi.deleteUser(email, cb)
       },
 
       modifyUser: function(email, data, cb) {
+        emitter.emit('log', {email:email, data:data, method:'modifyUser'})
         dbi.modifyUser(email, data, cb)
       },
 
@@ -45,6 +53,7 @@ module.exports = {
 
     })
 
+    s.emitter = emitter
     return s
 
   },
